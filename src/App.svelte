7206de0f7372a87f0379010d716d7bb3532b31d5
arrowtype <arrowtypeco@gmail.com>
2020-05-29 22:09:28 -0400

    make simple slider component and describe

---
+++ src/App.svelte
@@ -0,0 +1,51 @@
+<script>
+	import Slider from './Slider.svelte'
+
+	export let sliderValA = 0;
+	export let sliderValB = 15;
+</script>
+
+<main>
+	<h1>Simple SvelteJS slider component</h1>
+
+	<p>Slider A value is {sliderValA}</p>
+	<Slider bind:value={sliderValA} min="0" max="10" />
+
+	<p>Slider B value is {sliderValB}</p>
+	<Slider bind:value={sliderValB} min="0" max="30" />
+
+	<p>A + B is {sliderValA + sliderValB}</p>
+
+	<p>
+		This <code>Slider</code> component:
+	</p>
+	<ul>
+		<li>Has custom CSS which makes it easy to manipulate on a touch screen, while still looking simple & similar across platforms. This takes <a href="https://css-tricks.com/styling-cross-browser-compatible-range-inputs-css/">a lot of CSS</a> for a range input.</li>
+		<li>Is separate from the main app component, which is nice because it has custom CSS that only has to be written once. (It’s a component.)</li>
+		<li>Has props for value, min, & max.</li>
+		<li>Works just like a normal, plain-HTML range input does in Svelte. You can <a href="https://svelte.dev/tutorial/numeric-inputs">easily bind its value</a> to the state in the component it is used within).</li>
+	</ul>
+	<p>Note: this is intended more as a test & open example of how a Slider component can be made in SvelteJS. It is not necessarily representative of best practices or something that you should reuse without testing & improvement.</p>
+	<p>Have an idea of how to improve it? Please file an issue or make a pull request in the GitHub Repo!</p>
+</main>
+
+<style>
+	main {
+		height: 100%;
+		display: grid;
+		justify-content: center;
+		align-content: center;
+		line-break: 1.5;
+	}
+
+	a {
+		text-decoration: underline;
+	}
+	p, li {
+		max-width: 55ch;
+		margin-bottom: 1em;
+	}
+	code {
+		font-size: 1.4em;
+	}
+</style>
---
+++ src/Slider.svelte
@@ -0,0 +1,212 @@
+<script>
+    export let value;
+    export let min;
+    export let max;
+</script>
+
+<div class="slider-container">
+    <input type="number" bind:value={value} min={min} max={max}>
+    <span>{min}</span>
+    <input type="range" bind:value={value} min={min} max={max}>
+    <span>{max}</span>
+</div>
+
+<style>
+    :root {
+        --accent: #088;
+    }
+    .slider-container {
+        display: grid;
+        grid-template-columns: 6em min-content 1fr min-content;
+        grid-gap: 0.5em;
+        align-items: center;
+        margin: 1em 0;
+    }
+
+    input {
+        margin: 0;
+    }
+    
+    span {
+        opacity: 0.6;
+        font-size: 0.75em;
+    }
+
+    input[type="number"] {
+        padding-left: 1em;
+        margin-right: 1em;
+        font-feature-settings: 'tnum';
+        color: var(--accent);
+        font-weight: bold;
+    }
+
+    /* BETTER SLIDER CSS - TODO: split into separate component */
+    :root {
+        --trackBg: #bdbdbd;
+        --trackFocus: #cacaca;
+        --thumbBg: #088;
+        --trackHeight: 1px;
+        --thumbHeight: 12px;
+        --thumbTouchHeight: 48px;
+        --sliderComponentBg: #f4f4f4;
+    }
+  input[type="range"] {
+    -webkit-appearance: none;
+    margin: calc(1em - var(--thumbTouchHeight) / 2);
+    margin-left: calc(-1 * var(--thumbTouchHeight) / 2);
+    margin-right: calc(-1 * var(--thumbTouchHeight) / 2);
+    width: calc(100% + var(--thumbTouchHeight));
+    background: transparent; /* necessary for firefox */
+    border: none;
+  }
+
+  input[type="range"]:focus {
+    outline: none;
+    color: transparent;
+    text-shadow: 0 0 0 #000;
+  }
+  input[type="range"]::-webkit-slider-runnable-track {
+    width: 100%;
+    height: var(--trackHeight);
+    cursor: pointer;
+    background: transparent;
+    background-image: linear-gradient(
+        to right,
+        transparent,
+        transparent calc(var(--thumbTouchHeight) / 2),
+        var(--trackBg) calc(var(--thumbTouchHeight) / 2),
+        var(--trackBg) 50%,
+        transparent 50%
+      ),
+      linear-gradient(
+        to left,
+        transparent,
+        transparent calc(var(--thumbTouchHeight) / 2),
+        var(--trackBg) calc(var(--thumbTouchHeight) / 2),
+        var(--trackBg) 50%,
+        transparent 50%
+      );
+    border-radius: 1px; /* weidly, this property must be here */
+  }
+  input[type="range"]:focus::-webkit-slider-runnable-track {
+    outline: none;
+    background-image: linear-gradient(
+        to right,
+        transparent,
+        transparent calc(var(--thumbTouchHeight) / 2),
+        var(--trackFocus) calc(var(--thumbTouchHeight) / 2),
+        var(--trackFocus) 50%,
+        transparent 50%
+      ),
+      linear-gradient(
+        to left,
+        transparent,
+        transparent calc(var(--thumbTouchHeight) / 2),
+        var(--trackFocus) calc(var(--thumbTouchHeight) / 2),
+        var(--trackFocus) 50%,
+        transparent 50%
+      );
+  }
+  input[type="range"]::-webkit-slider-thumb {
+    height: var(--thumbTouchHeight);
+    width: var(--thumbTouchHeight);
+    border-radius: var(--thumbTouchHeight);
+    border: none;
+    cursor: pointer;
+    -webkit-appearance: none;
+    margin-top: calc(
+      -1 * (var(--thumbTouchHeight) / 2) + (var(--trackHeight) / 2)
+    );
+    background: none; /* necessary for iOS Safari */
+    background-image: radial-gradient(
+      var(--thumbBg) calc(var(--thumbHeight) / 2),
+      transparent calc(var(--thumbHeight) / 2),
+      transparent
+    );
+  }
+
+  input[type="range"]::-moz-range-track {
+    width: 100%;
+    height: var(--trackHeight);
+    border-radius: 1px;
+    cursor: pointer;
+    background-color: red;
+    background-image: linear-gradient(
+        to right,
+        var(--sliderComponentBg),
+        var(--sliderComponentBg) 20px,
+        var(--trackBg) 20px,
+        var(--trackBg) 50%,
+        transparent 50%
+      ),
+      linear-gradient(
+        to left,
+        var(--sliderComponentBg),
+        var(--sliderComponentBg) 20px,
+        var(--trackBg) 20px,
+        var(--trackBg) 50%,
+        transparent 50%
+      );
+  }
+  input[type="range"]:focus::-moz-range-track {
+    background-image: linear-gradient(
+        to right,
+        var(--sliderComponentBg),
+        var(--sliderComponentBg) 20px,
+        var(--trackFocus) 20px,
+        var(--trackFocus) 50%,
+        transparent 50%
+      ),
+      linear-gradient(
+        to left,
+        var(--sliderComponentBg),
+        var(--sliderComponentBg) 20px,
+        var(--trackFocus) 20px,
+        var(--trackFocus) 50%,
+        transparent 50%
+      );
+  }
+  input[type="range"]::-moz-focus-outer {
+    border: 0;
+  }
+  input[type="range"]::-moz-range-thumb {
+    height: var(--thumbTouchHeight);
+    width: var(--thumbTouchHeight);
+    border: none;
+    border-radius: var(--thumbTouchHeight);
+    background: none; /* necessary for iOS Safari */
+    background-image: radial-gradient(
+      var(--thumbBg) calc(var(--thumbHeight) / 2),
+      transparent calc(var(--thumbHeight) / 2),
+      transparent
+    );
+    cursor: pointer;
+  }
+
+  input[type="range"]::-ms-track {
+    width: calc(100%- var(--thumbTouchHeight));
+    margin-left: calc(var(--thumbTouchHeight) / 2);
+    height: var(--trackHeight);
+    cursor: pointer;
+  }
+  input[type="range"]::-ms-fill-lower {
+    background-color: #777;
+  }
+  input[type="range"]::-ms-fill-upper {
+    background: #777;
+  }
+  input[type="range"]::-ms-thumb {
+    width: var(--thumbHeight);
+    margin-left: calc(var(--thumbTouchHeight) / 2);
+    margin-right: calc(var(--thumbTouchHeight) / 2);
+    border-radius: 999px;
+    background-color: #222;
+    cursor: pointer;
+  }
+  input[type="range"]:focus::-ms-fill-lower {
+    background: blue;
+  }
+  input[type="range"]:focus::-ms-fill-upper {
+    background: blue;
+  }
+</style>
