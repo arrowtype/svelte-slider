b0ff0a6b7cb7277055bde165312e763f9b2ea9e8
arrowtype <arrowtypeco@gmail.com>
2020-05-29 22:33:37 -0400

    Clarifications to description

--- README.md
+++ README.md
@@ -2,14 +2,15 @@
 
 To use SvelteJS, please see the [SvelteJS Documentation](https://svelte.dev/).
 
-<p>
-    This <code>Slider</code> component:
-</p>
-<ul>
-    <li>Has custom CSS which makes it easy to manipulate on a touch screen, while still looking simple & similar across platforms. This takes <a href="https://css-tricks.com/styling-cross-browser-compatible-range-inputs-css/">a lot of CSS</a> for a range input.</li>
-    <li>Is separate from the main app component, which is nice because it has custom CSS that only has to be written once. (It’s a component.)</li>
-    <li>Has props for value, min, & max.</li>
-    <li>Works just like a normal, plain-HTML range input does in Svelte. You can <a href="https://svelte.dev/tutorial/numeric-inputs">easily bind its value</a> to the state in the component it is used within.</li>
-</ul>
-<p>Note: this is intended more as a test & open example of how a Slider component can be made in SvelteJS. It is not necessarily representative of best practices or something that you should reuse without testing & improvement.</p>
-<p>Have an idea of how to improve it? Please file an issue or make a pull request in the GitHub Repo!</p>
+---
+
+This `Slider` component:
+
+- Has custom CSS which makes it easy to manipulate on a touch screen, while still looking simple & similar across platforms. This takes [a lot of CSS](https://css-tricks.com/styling-cross-browser-compatible-range-inputs-css/) for a range input.
+- Is separate from the main app component, which is nice because it has custom CSS that only has to be written once. (It’s a component.)
+- Has props for value, min, max, & step.
+- Works just like a normal, plain-HTML range input does in Svelte. You can [easily bind its value](https://svelte.dev/tutorial/numeric-inputs) to the state in the component it is used within.
+
+**Note:** this is intended mostly as a test & shared example of how a Slider component can be made in SvelteJS. It is not necessarily representative of best practices in Svelte, and it is probably not something that you should reuse without testing & improvement.
+
+Have an idea of how to improve it? Please file an issue or make a pull request in the GitHub Repo!</p>
--- src/App.svelte
+++ src/App.svelte
@@ -6,15 +6,17 @@
 </script>
 
 <main>
-	<h1>Simple SvelteJS slider component</h1>
+	<h1>A simple SvelteJS slider component</h1>
 
 	<p>Slider A value is {sliderValA}</p>
-	<Slider bind:value={sliderValA} min="0" max="10" />
+	<Slider bind:value={sliderValA} min="0" max="10" step="1"/>
 
 	<p>Slider B value is {sliderValB}</p>
-	<Slider bind:value={sliderValB} min="0" max="30" />
+	<Slider bind:value={sliderValB} min="0" max="30" step="5"/>
 
 	<p>A + B is {sliderValA + sliderValB}</p>
+
+	<h2>Description</h2>
 
 	<p>
 		This <code>Slider</code> component:
@@ -22,28 +24,36 @@
 	<ul>
 		<li>Has custom CSS which makes it easy to manipulate on a touch screen, while still looking simple & similar across platforms. This takes <a href="https://css-tricks.com/styling-cross-browser-compatible-range-inputs-css/">a lot of CSS</a> for a range input.</li>
 		<li>Is separate from the main app component, which is nice because it has custom CSS that only has to be written once. (It’s a component.)</li>
-		<li>Has props for value, min, & max.</li>
+		<li>Has props for value, min, max, & step.</li>
 		<li>Works just like a normal, plain-HTML range input does in Svelte. You can <a href="https://svelte.dev/tutorial/numeric-inputs">easily bind its value</a> to the state in the component it is used within.</li>
 	</ul>
-	<p>Note: this is intended more as a test & open example of how a Slider component can be made in SvelteJS. It is not necessarily representative of best practices or something that you should reuse without testing & improvement.</p>
-	<p>Have an idea of how to improve it? Please file an issue or make a pull request in the <a href="https://github.com/arrowtype/svelte-slider">GitHub Repo!</a></p>
+
+	<p><b>Note:</b> this is intended mostly as a test & shared example of how a Slider component can be made in SvelteJS. It is not necessarily representative of best practices in Svelte, and it is probably not something that you should reuse without testing & improvement.</p>
+
+	<p>Have an idea of how to improve this? Please file an issue or make a pull request in the <a href="https://github.com/arrowtype/svelte-slider">GitHub Repo!</a></p>
+
+	<p>Made by <a href="https://arrowtype.com">@ArrowType</a></p>
 </main>
 
 <style>
 	main {
-		height: 100%;
-		display: grid;
-		justify-content: center;
-		align-content: center;
-		line-break: 1.5;
+		margin: 0 auto;
+		max-width: 55ch;
+		padding: 5vmin 0.5rem;
+		line-height: 1.5;
 	}
-
-	a {
-		text-decoration: underline;
+	h1 {
+		margin-bottom: 3rem;
+	}
+	h2 {
+		margin-top: 4rem;
 	}
 	p, li {
 		max-width: 55ch;
 		margin-bottom: 1em;
+	}
+	a {
+		text-decoration: underline;
 	}
 	code {
 		font-size: 1.4em;
